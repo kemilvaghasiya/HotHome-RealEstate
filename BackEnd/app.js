c4f675490459234ebaa07b4e3df2d09ec8fb85eb
hiren kachhadiya <hirenkachhadiya6666@gmail.com>
2023-03-19 12:57:37 +0530

    add and get images add in backend side.

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,34 +1,45 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-var bodyParser = require('body-parser')
+const express = require("express");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+var bodyParser = require("body-parser");
+var fs = require("fs");
+const os = require("os");
+var multer = require("multer");
+var storage = multer.memoryStorage();
+var upload_storage = multer({ storage: storage });
+var upload = multer({ storage });
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 const enableCors = (req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
 };
 app.use(enableCors);
 
 // Connect to the database
-mongoose.connect('mongodb://localhost/real-estate', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.log(err));
+mongoose
+  .connect("mongodb://localhost/real-estate", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
 // Create a schema for user data
 const userSchema = new mongoose.Schema({
   name: String,
   email: String,
   password: String,
-  phone: String
+  phone: String,
 });
 
 const propertySchema = new mongoose.Schema({
@@ -37,57 +48,75 @@ const propertySchema = new mongoose.Schema({
   noOfUnits: String,
   floorPlan: String,
   pincode: Number,
+  price: Number,
+  image: [{ type: Schema.Types.ObjectId, ref: "Image" }],
+});
+
+const imageSchema = new mongoose.Schema({
+  fileName: String,
+  fileType: String,
+  fileSize: Number,
+  imageData: Buffer,
+  propertyId: String,
 });
 
 // Create a model using the schema
-const User = mongoose.model('User', userSchema);
-
+const User = mongoose.model("User", userSchema);
+const Property = mongoose.model("Property", propertySchema);
+const Image = mongoose.model("Image", imageSchema);
 // Define a route for user signup
-app.post('/signup', async (req, res) => {
+app.post("/signup", async (req, res) => {
   const { name, email, password, phone } = req.body;
   try {
     // Check if user already exists
     const existingUser = await User.findOne({ email });
     if (existingUser) {
-      return res.status(400).json({ message: 'User already exists' });
+      return res.status(400).json({ message: "User already exists" });
     }
     // Create a new user
     const hashedPassword = await bcrypt.hash(password, 12);
     const newUser = new User({ name, email, password: hashedPassword, phone });
     await newUser.save();
-    res.status(201).json({ message: 'User created successfully' });
+    res.status(201).json({ message: "User created successfully" });
   } catch (err) {
-    console.log(err)
+    console.log(err);
     res.status(500).json({ message: err.message });
   }
 });
 
 // Define a route for user login
-app.post('/login', async (req, res) => {
+app.post("/login", async (req, res) => {
   const { email, password } = req.body;
   try {
     // Check if user exists
     const existingUser = await User.findOne({ email });
     if (!existingUser) {
-      return res.status(400).json({ message: 'Invalid credentials' });
+      return res.status(400).json({ message: "Invalid credentials" });
     }
     // Check if password is correct
-    const isPasswordCorrect = await bcrypt.compare(password, existingUser.password);
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
     if (!isPasswordCorrect) {
-      return res.status(400).json({ message: 'Invalid credentials' });
+      return res.status(400).json({ message: "Invalid credentials" });
     }
     // Create a JWT token
-    const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, 'mysecret', { expiresIn: '12h' });
-    res.status(200).json({ user:existingUser,token });
+    const token = jwt.sign(
+      { email: existingUser.email, id: existingUser._id },
+      "mysecret",
+      { expiresIn: "12h" }
+    );
+    res.status(200).json({ user: existingUser, token });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
 });
 
 //Register property
-app.post("/register-properties", async (req, res) => {
+app.post("/register-properties", upload.any("images"), async (req, res) => {
   const propertyData = req.body;
-  console.log(propertyData);
+  const files = req.files;
   if (!propertyData.propertyName) {
     return res.status(400).json({ message: "propertyName is required." });
   }
@@ -100,13 +129,49 @@ app.post("/register-properties", async (req, res) => {
   if (!propertyData.floorPlan) {
     return res.status(400).json({ message: "floorPlan is required." });
   }
-  if (!propertyData.pincode && Number(propertyData.pincode)) {
+  if (!propertyData.pincode && typeof propertyData.pincode != Number) {
     return res.status(400).json({ message: "pincode is invalid." });
   }
-
+  if (!propertyData.price && typeof propertyData.price != Number) {
+    return res.status(400).json({ message: "price is required." });
+  }
   const property = new Property(propertyData);
   await property.save();
+
+  const imageIds = [];
+
+  for (let file of files) {
+    const imageData = new Image({
+      fileName: file.originalname,
+      fileType: file.mimetype,
+      fileSize: file.size,
+      imageData: file.buffer,
+      propertyId: property._id,
+    });
+    const images = new Image(imageData);
+    await images.save();
+    imageIds.push(images._id);
+  }
+  await Property.findOneAndUpdate(
+    { _id: property._id },
+    { image: imageIds },
+    { new: true }
+  );
+
   res.send(property);
+});
+
+//get property image
+app.get("/get-property-images/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const [image] = await Image.find({ _id: id });
+    res.contentType(image.fileType);
+    res.send(image.imageData);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: "Internal server error." });
+  }
 });
 
 //get all properties
@@ -137,7 +202,7 @@ app.put("/update-property/:id", async (req, res) => {
 });
 
 //delete property by id
-app.delete('/delete-property/:id', async (req, res) => {
+app.delete("/delete-property/:id", async (req, res) => {
   try {
     const property = await Property.findByIdAndDelete(req.params.id);
     if (!property) {
@@ -145,11 +210,11 @@ app.delete('/delete-property/:id', async (req, res) => {
     }
     res.send(property);
   } catch (err) {
-    return res.status(500).json({ message: "Internal server error." });;
+    return res.status(500).json({ message: "Internal server error." });
   }
-})
+});
 
 // Start the server
 app.listen(5000, () => {
-  console.log('Server started on port 5000');
+  console.log("Server started on port 5000");
 });
